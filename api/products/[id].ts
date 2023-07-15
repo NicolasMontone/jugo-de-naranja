27597053e40a7a93ff3fd1426b0b4708636bc84c
NicolasMontone <montonenicolas01@gmail.com>
2023-07-15 16:44:42 -0300

    fix: handle possible errors.

--- api/products/[id].ts
+++ api/products/[id].ts
@@ -16,21 +16,38 @@ const handler: VercelApiHandler = async (
   }
 
   const code = req.query.id as string
+  try {
+    const { data: productResponse } = await axios.get<ProductResponse>(
+      `https://go-upc.com/api/v1/code/${code}?key=${process.env.GO_UPC_API_KEY}`,
+    )
 
-  const { data: productResponse } = await axios.get<ProductResponse>(
-    `https://go-upc.com/api/v1/code/${code}?key=${process.env.GO_UPC_API_KEY}`,
-  )
+    const product = productResponse.product
 
-  const product = productResponse.product
+    res.status(200).json({
+      name: product.name,
+      description: product.description,
+      imageUrl: product.imageUrl,
+      brand: product.brand,
+      specs: product.specs,
+      category: product.category,
+    })
+    return
+  } catch (error: any) {
+    if (error.response?.status === 400) {
+      res.status(400).json({ error: 'Invalid code' })
+      return
+    }
 
-  res.status(200).json({
-    name: product.name,
-    description: product.description,
-    imageUrl: product.imageUrl,
-    brand: product.brand,
-    specs: product.specs,
-    category: product.category,
-  })
+    if (error.response?.status === 401) {
+      res.status(401).json({ error: 'Unauthorized' })
+      return
+    }
+
+    if (error.response?.status === 404) {
+      res.status(404).json({ error: 'Product not found' })
+      return
+    }
+  }
 }
 
 export default handler
